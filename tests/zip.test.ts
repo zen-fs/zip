17ab526b168ac07f0775cba150408f24a3245116
James Prevett <jp@jamespre.dev>
2024-10-02 21:52:10 -0500

    Change to using `assert`

--- tests/zip.test.ts
+++ tests/zip.test.ts
@@ -1,10 +1,10 @@
 import { configureSingle, fs } from '@zenfs/core';
 import { readFileSync } from 'fs';
+import assert from 'node:assert';
+import { suite, test } from 'node:test';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { Zip } from '../src/ZipFS.js';
-import { test, suite } from 'node:test';
-import { equal } from 'node:assert';
 
 suite('Basic ZIP operations', () => {
 	test('Configure', async () => {
@@ -14,22 +14,22 @@ suite('Basic ZIP operations', () => {
 	});
 
 	test('readdir /', () => {
-		equal(fs.readdirSync('/').length, 3);
+		assert(fs.readdirSync('/').length == 3);
 	});
 
 	test('read /one.txt', () => {
-		equal(fs.readFileSync('/one.txt', 'utf8'), '1');
+		assert(fs.readFileSync('/one.txt', 'utf8') == '1');
 	});
 
 	test('read /two.txt', () => {
-		equal(fs.readFileSync('/two.txt', 'utf8'), 'two');
+		assert(fs.readFileSync('/two.txt', 'utf8') == 'two');
 	});
 
 	test('readdir /nested', () => {
-		equal(fs.readdirSync('/nested').length, 1);
+		assert(fs.readdirSync('/nested').length == 1);
 	});
 
 	test('readdir /nested/omg.txt', () => {
-		equal(fs.readFileSync('/nested/omg.txt', 'utf8'), 'This is a nested file!');
+		assert(fs.readFileSync('/nested/omg.txt', 'utf8') == 'This is a nested file!');
 	});
 });
